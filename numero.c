5ccf95418f080afcbeac3f74ccb9861b6361c2d0
Laure <laurentavares524@gmail.com>
2026-04-07 19:55:54 -0300

    Add files via upload

---
+++ numero.c
@@ -0,0 +1,22 @@
+//fazer um programa para imprimir se um numero eh positivo, negativo ou igual a zero
+
+
+#include <stdio.h>
+
+int main() 
+{
+    int numero;
+
+    printf("Digite um numero:\n");
+    scanf(" %d", &numero);
+
+    if(numero == 0)
+        prinft("Numero igual a ZERO");
+
+    else if(numero < 0)
+        prinft("Numero menor que ZERO");
+
+    else
+        prinft("Numero menor que ZERO");
+    
+}
