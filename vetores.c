c6323d4d860843860024eabe3747654c05d2521a
Laure <laurentavares524@gmail.com>
2026-05-05 19:56:07 -0300

    Add files via upload

---
+++ vetores.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int vetor [10];
+    int i; //indíce para controle do loop
+
+    for(i=0; i<10; i++);
+    {
+        vetor[i]=i+1;
+        printf("%d", vetor[i]);
+
+}
+    printf("\nFim do programa.\n");
+}
+
+
